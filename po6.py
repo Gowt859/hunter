29b31d2fd05f1fa0ba9ee8d8d6d1e09be013ce6a
Gowt859 <52487557+Gowt859@users.noreply.github.com>
2019-08-07 14:44:48 +0530

    Create po6.py

---
+++ po6.py
@@ -0,0 +1,14 @@
+n1=int(input())
+ls[]
+for t=list(map(int,input().split()))
+lst.sort()
+r1=i in range(len(lst)-1):
+    if lst[i]==lst[i+1]:
+        r1.append(lst[i])
+
+if r1:
+    for j in set(r1):
+        print(j,end=" ")
+        break
+else:
+    print("unique")        
