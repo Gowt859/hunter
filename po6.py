5056c6fe3f2a54400aa37cef40694e6b490f53f5
Gowt859 <52487557+Gowt859@users.noreply.github.com>
2019-08-07 14:47:26 +0530

    Update po6.py

--- po6.py
+++ po6.py
@@ -2,13 +2,13 @@ n1=int(input())
 ls[]
 for t=list(map(int,input().split()))
 lst.sort()
-r1=i in range(len(lst)-1):
-    if lst[i]==lst[i+1]:
-        r1.append(lst[i])
+r1=j in range(len(lst)-1):
+    if lst[j]==lst[j+1]:
+        r1.append(lst[j])
 
 if r1:
-    for j in set(r1):
-        print(j,end=" ")
+    for k in set(r1):
+        print(k,end=" ")
         break
 else:
     print("unique")        
